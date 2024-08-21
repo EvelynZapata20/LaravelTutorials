d945a6bfa8c4c1ca8408ba2002296bb76f828652
Evelyn <eazapatat@eafit.edu.co>
2024-08-21 08:49:36 -0500

    fix: execute pint to final version of tutorial 1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,22 +15,24 @@ class HomeController extends Controller
     public function about(): View
     {
         $viewData = [];
-        $viewData["title"] = "About us - Online Store";
-        $viewData["subtitle"] = "About us";
-        $viewData["description"] = "This is an about page ... ";
-        $viewData["author"] = "Developed by: Evelyn Zapata";
-        return view('home.about')->with("viewData", $viewData);
-    }    
+        $viewData['title'] = 'About us - Online Store';
+        $viewData['subtitle'] = 'About us';
+        $viewData['description'] = 'This is an about page ... ';
+        $viewData['author'] = 'Developed by: Evelyn Zapata';
+
+        return view('home.about')->with('viewData', $viewData);
+    }
 
     // Activity 2
     public function contact(): View
     {
         $viewData = [];
-        $viewData["title"] = "Contact us - Online Store";
-        $viewData["subtitle"] = "Contact us";
-        $viewData["email"] = "online_store@gmail.com";
-        $viewData["address"] = "P. Sherman, 42 Wallaby Way, Sydney, Australia";
-        $viewData["phone"] = "04 1234 5678";
-        return view('home.contact')->with("viewData", $viewData);
+        $viewData['title'] = 'Contact us - Online Store';
+        $viewData['subtitle'] = 'Contact us';
+        $viewData['email'] = 'online_store@gmail.com';
+        $viewData['address'] = 'P. Sherman, 42 Wallaby Way, Sydney, Australia';
+        $viewData['phone'] = '04 1234 5678';
+
+        return view('home.contact')->with('viewData', $viewData);
     }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,48 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
-use Illuminate\Http\RedirectResponse;
 
 class ProductController extends Controller
 {
-
     public static $products = [
-        ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "price"=>50],
-        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "price"=>7000000],
-        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "price"=>2300000],
-        ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "price"=>700000]
+        ['id' => '1', 'name' => 'TV', 'description' => 'Best TV', 'price' => 50],
+        ['id' => '2', 'name' => 'iPhone', 'description' => 'Best iPhone', 'price' => 7000000],
+        ['id' => '3', 'name' => 'Chromecast', 'description' => 'Best Chromecast', 'price' => 2300000],
+        ['id' => '4', 'name' => 'Glasses', 'description' => 'Best Glasses', 'price' => 700000],
     ];
 
     public function index(): View
     {
         $viewData = [];
-        $viewData["title"] = "Products - Online Store";
-        $viewData["subtitle"] = "List of products";
-        $viewData["products"] = ProductController::$products;
-        return view('product.index')->with("viewData", $viewData);
+        $viewData['title'] = 'Products - Online Store';
+        $viewData['subtitle'] = 'List of products';
+        $viewData['products'] = ProductController::$products;
+
+        return view('product.index')->with('viewData', $viewData);
     }
 
-    public function show(string $id) : View | RedirectResponse
+    public function show(string $id): View|RedirectResponse
     {
         $viewData = [];
         // Activity 5
-        if (!isset(ProductController::$products[$id-1])) {
+        if (! isset(ProductController::$products[$id - 1])) {
             return redirect()->route('home.index');
         }
-        $product = ProductController::$products[$id-1];
-        $viewData["title"] = $product["name"]." - Online Store";
-        $viewData["subtitle"] = $product["name"]." - Product information";
-        $viewData["product"] = $product;
-        return view('product.show')->with("viewData", $viewData);
+        $product = ProductController::$products[$id - 1];
+        $viewData['title'] = $product['name'].' - Online Store';
+        $viewData['subtitle'] = $product['name'].' - Product information';
+        $viewData['product'] = $product;
+
+        return view('product.show')->with('viewData', $viewData);
     }
 
     public function create(): View
     {
         $viewData = []; //to be sent to the view
-        $viewData["title"] = "Create product";
-        return view('product.create')->with("viewData",$viewData);
+        $viewData['title'] = 'Create product';
+
+        return view('product.create')->with('viewData', $viewData);
     }
 
     // Activity 7:
@@ -54,10 +56,11 @@ class ProductController extends Controller
     public function save(Request $request)
     {
         $productData = $request->validate([
-            "name" => "required",
-            "price" => "required|numeric|gt:0"
+            'name' => 'required',
+            'price' => 'required|numeric|gt:0',
         ]);
-        return view('product.save')->with("productData", $productData);
+
+        return view('product.save')->with('productData', $productData);
         //dd($request->all());
 
         //here will be the code to call the model and save it to the database
