fcc9fce64de7db42501db8595b8e7e23cd3c7ab4
Evelyn <eazapatat@eafit.edu.co>
2024-09-24 21:00:34 -0500

    feat: add tutorial 04 activity 2

--- app/Http/Controllers/Api/ProductApiController.php
+++ app/Http/Controllers/Api/ProductApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class ProductApiController extends Controller
 {
@@ -18,5 +19,17 @@ class ProductApiController extends Controller
     {
         $product = Product::findOrFail($id);
         return response()->json($product, 200);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        Product::validate($request);
+
+        $newProduct = new Product();
+        $newProduct->setName($request->input('name'));
+        $newProduct->setPrice($request->input('price'));
+        $newProduct->save();
+
+        return response()->json(['message' => 'Product created successfully', 'product' => $newProduct], 201);
     }
 }
